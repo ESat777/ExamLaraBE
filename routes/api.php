0f3d62af76128ab7ad55adebda186844a5b28d8e
Edis <47144455+ESat777@users.noreply.github.com>
2022-11-24 09:34:06 +0200

    add: controllers, routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
-
+use App\Http\Controllers\SchoolsController;
+use App\Http\Controllers\AppliController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PassportAuthController;
@@ -20,3 +21,15 @@ Route::post('register', [PassportAuthController::class, 'register']);
 Route::post('login', [PassportAuthController::class, 'login']);
 Route::post('logout', [PassportAuthController::class, 'logout'])->middleware('auth:api');
 Route::get('authcheck', [PassportAuthController::class, 'index'])->middleware('auth:api');
+
+Route::get('schools', [SchoolsController::class, 'index']);
+Route::get('schools/{id}', [SchoolsController::class, 'show']);
+Route::post('schools', [SchoolsController::class, 'store'])->middleware('auth:api');
+Route::post('schools/{id}', [SchoolsController::class, 'update'])->middleware('auth:api');
+Route::delete('schools/{id}', [SchoolsController::class, 'destroy'])->middleware('auth:api');
+
+Route::get('applications', [AppliController::class, 'index'])->middleware('auth:api');
+Route::get('applications/all', [AppliController::class, 'all'])->middleware('auth:api');
+Route::get('applications/{id}', [AppliController::class, 'status'])->middleware('auth:api');
+Route::post('applications', [AppliController::class, 'store'])->middleware('auth:api');
+Route::delete('applications/{id}', [AppliController::class, 'destroy'])->middleware('auth:api');
