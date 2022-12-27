16d71bde434763624fb84c217d4f5246d0b888e3
Edis <47144455+ESat777@users.noreply.github.com>
2022-12-27 10:35:40 +0200

    add new table details

--- database/migrations/2022_11_24_074136_create_applis_table.php
+++ database/migrations/2022_11_24_074136_create_applis_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->integer('approved')->default(0);
             $table->string('student_id')->nullable();
+            // $table->float('personalCode', 9, 0) ;
             $table->string('class')->nullable();
             $table->string('name')->nullable();
             $table->string('surname')->nullable();
